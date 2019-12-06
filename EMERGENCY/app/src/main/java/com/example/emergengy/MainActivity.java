2dd1ecde6e83fc0c35b0280859a30a28531ebf06
Prabhas Mondal <44371847+pmondal98@users.noreply.github.com>
2019-12-06 19:55:30 +0530

    variable b created.

--- EMERGENCY/app/src/main/java/com/example/emergengy/MainActivity.java
+++ EMERGENCY/app/src/main/java/com/example/emergengy/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a=10;
+        int b=10;
     }
 }
