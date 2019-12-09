aaea4d3febd0da9ac21bd04d40a04204b2969c2a
debdip2209 <58582774+debdip2209@users.noreply.github.com>
2019-12-10 02:14:52 +0530

    Some UI designs are updated.

--- EMERGENCY/app/src/main/java/com/example/emergengy/SplashScreen.java
+++ EMERGENCY/app/src/main/java/com/example/emergengy/SplashScreen.java
@@ -19,7 +19,7 @@ public class SplashScreen extends AppCompatActivity {
 
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                Intent i = new Intent(SplashScreen.this, login.class);
                 startActivity(i);
 
                 finish();
--- EMERGENCY/app/src/main/java/com/example/emergengy/login.java
+++ EMERGENCY/app/src/main/java/com/example/emergengy/login.java
@@ -2,13 +2,30 @@ package com.example.emergengy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
+    Button btnlogin;
+    TextView tvregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnlogin=findViewById(R.id.btnlogin);
+        tvregister=findViewById(R.id.tvregister);
+
+        tvregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(login.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
