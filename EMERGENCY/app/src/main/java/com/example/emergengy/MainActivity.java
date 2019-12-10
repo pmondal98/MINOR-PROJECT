6bc96b10cfb55a0992543717b37ce06b086465ad
Prabhas Mondal <44371847+pmondal98@users.noreply.github.com>
2019-12-11 04:30:25 +0530

    Login and Registration done fully(Java code and UI). All ready.

--- EMERGENCY/app/src/main/java/com/example/emergengy/MainActivity.java
+++ EMERGENCY/app/src/main/java/com/example/emergengy/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.emergengy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etname,etemail,etpassword,etconfirmpassword,etage,etaddress,etpin,etcontactnumber;
+    private EditText etname,etemail,etpassword,etage,etaddress,etpin,etcontactnumber;
+    private Button btnsubmit;
+    DatabaseReference ref;
+    FirebaseAuth mfirebaseauth;
+    Member member;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +35,88 @@ public class MainActivity extends AppCompatActivity {
 
         etname=findViewById(R.id.etname);
         etemail=findViewById(R.id.etemail);
+        etpassword=findViewById(R.id.etpassword);
+        etage=findViewById(R.id.etage);
+        etaddress=findViewById(R.id.etaddress);
+        etpin=findViewById(R.id.etpin);
+        etcontactnumber=findViewById(R.id.etcontactnumber);
+        btnsubmit=findViewById(R.id.btnsubmit);
+
+        mfirebaseauth = FirebaseAuth.getInstance();
+        member=new Member();
+//        ref= FirebaseDatabase.getInstance().getReference().child("USERS");
 
 
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(etname.getText().toString().trim().length()==0)
+                    etname.setError("Name is required");
+                else if (etemail.getText().toString().trim().length()==0)
+                    etemail.setError("Email is required");
+                else if (etpassword.getText().toString().trim().length()==0)
+                    etpassword.setError("Password is required");
+                else if(etage.getText().toString().trim().length()==0)
+                    etage.setError("Age is required");
+                else if (etaddress.getText().toString().trim().length()==0)
+                    etaddress.setError("Address is required");
+                else if (etpin.getText().toString().trim().length()==0)
+                    etpin.setError("Pic Code of your area is required");
+                else if (etcontactnumber.getText().toString().trim().length()==0)
+                    etcontactnumber.setError("Phone number is required");
+                else {
+                    CreateUserAndSaveData();
+                }
+            }
+        });
+
+    }
+
+    private void CreateUserAndSaveData() {
+        mfirebaseauth.createUserWithEmailAndPassword(etemail.getText().toString(),etpassword.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    saveData();
+                    Toast.makeText(MainActivity.this, "User Creation.....Sucessfull", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    FirebaseAuthException e = (FirebaseAuthException)task.getException();
+                    Log.i("ERROR",e.getMessage());
+                    Toast.makeText(MainActivity.this, "User Creation Failed....Please try again later", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void saveData()
+    {
+
+        member.setName(etname.getText().toString().trim());
+        member.setEmail(etemail.getText().toString().trim());
+        member.setPassword(etpassword.getText().toString().trim());
+        member.setAge(etage.getText().toString().trim());
+        member.setAddress(etaddress.getText().toString().trim());
+        member.setPin(etpin.getText().toString().trim());
+        member.setContactnumber(etcontactnumber.getText().toString().trim());
+
+        ref= FirebaseDatabase.getInstance().getReference().child("USERS").child(mfirebaseauth.getCurrentUser().getUid());
+
+        ref.setValue(member);
+
+        Toast.makeText(this, "Registration Successful!!!", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(MainActivity.this,login.class));
+
+        etname.setText(null);
+        etemail.setText(null);
+        etemail.setText(null);
+        etpassword.setText(null);
+        etage.setText(null);
+        etaddress.setText(null);
+        etpin.setText(null);
+        etcontactnumber.setText(null);
     }
 }
--- EMERGENCY/app/src/main/java/com/example/emergengy/login.java
+++ EMERGENCY/app/src/main/java/com/example/emergengy/login.java
@@ -1,24 +1,82 @@
 package com.example.emergengy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
 
+    EditText etemail,etpassword;
     Button btnlogin;
     TextView tvregister;
+    FirebaseAuth mfirebaseauth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        etemail=findViewById(R.id.etemail);
+        etpassword=findViewById(R.id.etpassword);
         btnlogin=findViewById(R.id.btnlogin);
         tvregister=findViewById(R.id.tvregister);
+
+        mfirebaseauth = FirebaseAuth.getInstance();
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=etemail.getText().toString().trim();
+                String password=etpassword.getText().toString().trim();
+                if(email.isEmpty())
+                {
+                    etemail.setError("Please enter email id");
+                    etemail.requestFocus();
+                }
+                else if(password.isEmpty())
+                {
+                    etpassword.setError("Please enter password");
+                    etpassword.requestFocus();
+                }
+                else if(email.isEmpty() && password.isEmpty())
+                {
+                    Toast.makeText(login.this, "Login failed....please fill up the details", Toast.LENGTH_LONG).show();
+                }
+                else if(!(email.isEmpty() && password.isEmpty()))
+                {
+                    mfirebaseauth.signInWithEmailAndPassword(email,password).addOnCompleteListener(login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful())
+                            {
+                                Toast.makeText(login.this, "Login Unsuccessful....please try again later", Toast.LENGTH_LONG).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(login.this, "Login Successful....", Toast.LENGTH_SHORT).show();
+                                //startActivity(new Intent(login.this,HomeActivity.class));
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(login.this, "Error Occured....please try again later", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         tvregister.setOnClickListener(new View.OnClickListener() {
             @Override
