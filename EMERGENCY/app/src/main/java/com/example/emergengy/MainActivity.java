6fcbb90e33ce5fbb3196291c98b1d641fdbb1834
Prabhas Mondal <44371847+pmondal98@users.noreply.github.com>
2019-12-10 22:35:16 +0530

    UI designed updated

--- EMERGENCY/app/src/main/java/com/example/emergengy/MainActivity.java
+++ EMERGENCY/app/src/main/java/com/example/emergengy/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.emergengy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etname,etemail,etpassword,etconfirmpassword,etage,etaddress,etpin,etcontactnumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etname=findViewById(R.id.etname);
+        etemail=findViewById(R.id.etemail);
+
 
     }
 }
